26977995ca61ef282bed3b3a63186d5b5f9f9b2f
rohini kushwaha <rohiniee0028@gmail.com>
2025-11-03 00:27:14 +0530

    Implemented Header, Restaurant cards

--- App.js
+++ App.js
@@ -1,8 +1,170 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const heading = React.createElement("h1", {id:"title"}, "Hello World from React");
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=&txt_keyword=All' alt='company-logo' className="logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = (props) => {
+    const { name, cusines, rating, duration, cost, img} = props.resData;
+    return (
+        <div className="res-card" >
+            <img className="res-logo" src={img} alt={name} />
+            <div className="res-details">
+                <h3>{name}</h3>
+                <h4>{cusines}</h4>
+                <p className="rating">{rating}★</p>
+                <p className="duration">{duration}</p>
+                <p className="cost">Price: ₹{(cost/100).toLocaleString()}</p>
+            </div>
+        </div>
+    )
+}
+
+const resList = [
+    {
+        id: 1,
+        name: "Meghna Foods",
+        cusines: "North Indian, Biryani",
+        rating: "4.4",
+        duration: "30 mins",
+        cost : 40000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/1/13/7755cfa2-4bce-47ca-af36-d97dbfc7a157_af8bb4a8-2eee-428a-b664-7e38f498b5bf.jpg"
+    },
+    {
+        id: 2,
+        name: "Green Bowl",
+        cusines: "Salads, Healthy",
+        rating: "4.7",
+        duration: "20 mins",
+        cost: 35000,
+        img: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=800&q=80&auto=format&fit=crop&ixlib=rb-4.0.3&s=0a6b3a0a1f3b3f4d1a2c5d6e7f8a9b0c"
+    },
+    {
+        id: 3,
+        name: "Pasta Palace",
+        cusines: "Italian, Pasta",
+        rating: "4.2",
+        duration: "35 mins",
+        cost: 50000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/6/30/567fe7fb-5d26-4d48-b485-6499740981e1_1cd6f0d4-986a-499f-a89c-0d6c812f11d1.jpg_compressed"
+    },
+    {
+        id: 4,
+        name: "Spice Hub",
+        cusines: "Chinese, Indo-Chinese",
+        rating: "4.0",
+        duration: "25 mins",
+        cost: 25000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/4/9/0b80cf19-1b13-4893-b8fb-d88124d34d0f_29f94631-155f-4f2a-bd99-b3a7ea17f17d.png_compressed"
+    },
+    {
+        id: 5,
+        name: "Sushi World",
+        cusines: "Japanese, Sushi",
+        rating: "4.8",
+        duration: "40 mins",
+        cost: 80000,
+        img: "https://images.unsplash.com/photo-1553621042-f6e147245754?w=800&q=80&auto=format&fit=crop&ixlib=rb-4.0.3&s=3b2c1d0e9f8a7b6c5d4e3f2012345678"
+    }
+    ,
+    {
+        id: 6,
+        name: "Taco Town",
+        cusines: "Mexican, Tacos",
+        rating: "4.3",
+        duration: "22 mins",
+        cost: 30000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2025/3/31/d4e08de4-bf55-4777-afab-7fc227c4ffe8_49b9d160-df1d-4190-91cf-e876e919a54f.png"
+    },
+    {
+        id: 7,
+        name: "The Dosa Corner",
+        cusines: "South Indian, Breakfast",
+        rating: "4.6",
+        duration: "18 mins",
+        cost: 22000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/iyymp6ylvrbrcmfzbw3o"
+    },
+    {
+        id: 8,
+        name: "Midnight Pizza",
+        cusines: "Pizza, Fast Food",
+        rating: "4.1",
+        duration: "28 mins",
+        cost: 42000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/7/18/e583bc07-410f-4f27-b7a1-5fd3bb72ddfa_169faa36-e46f-4e5b-b720-863ca0246289.jpg",
+    },
+    {
+        id: 9,
+        name: "Vegan Delight",
+        cusines: "Vegan, Healthy",
+        rating: "4.5",
+        duration: "24 mins",
+        cost: 37000,
+        img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/25964f84f1523df7e07f294ebb03e446",
+    },
+    {
+        id: 10,
+        name: "Cafe Mocha",
+        cusines: "Cafe, Bakery",
+        rating: "4.2",
+        duration: "15 mins",
+        cost: 18000,
+        img: "https://images.unsplash.com/photo-1498804103079-a6351b050096?w=800&q=80&auto=format&fit=crop&ixlib=rb-4.0.3&s=1234567890abcdef1234"
+    },
+    {
+        id: 11,
+        name: "Kebabi Express",
+        cusines: "Kebabs, North Indian",
+        rating: "4.0",
+        duration: "32 mins",
+        cost: 45000,
+        img: "https://images.unsplash.com/photo-1544025162-d76694265947?w=800&q=80&auto=format&fit=crop&ixlib=rb-4.0.3&s=0fedcba9876543210fed"
+    }
+]
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                <input type="search" placeholder="search food" className="search_input" />
+            </div>
+            <div className="res-container">
+                {resList.map((restaurant) => (
+                    <RestaurantCard key={restaurant.id} resData={restaurant} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+// not using keys (not acceptable) <<<<< index as keys <<<<<<<<<<<<<<< unique id as keys (best practice)
+
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<AppLayout />);
