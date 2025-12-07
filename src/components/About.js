a698e9048b899cbfdb1260f937b2065f3b385037
rohini kushwaha <rohiniee0028@gmail.com>
2025-12-07 22:58:39 +0530

    updated user class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import UserClass from './UserClass';
 
 const About = () => {
   return (
     <div className='component-top-spacing'>
       <h1>About us</h1>
       <h2>This is namaste React Web series</h2>
+      <hr/>
+      <UserClass name={'Rohini (class)'} location={'Gurgaon'} contact={'rohini19394@gmail.com'}/>
     </div>
   );
 }
