bcd46adf20d61b05d5929cb6b3407a34cf6cf0f3
rohini kushwaha <rohiniee0028@gmail.com>
2025-12-28 23:34:28 +0530

    life cycle method

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import UserClass from './UserClass';
 
-const About = () => {
-  return (
-    <div className='component-top-spacing'>
-      <h1>About us</h1>
-      <h2>This is namaste React Web series</h2>
-      <hr/>
-      <UserClass name={'Rohini (class)'} location={'Gurgaon'} contact={'rohini19394@gmail.com'}/>
-    </div>
-  );
+// const About = () => {
+//   return (
+//     <div className='component-top-spacing'>
+//       <h1>About us</h1>
+//       <h2>This is namaste React Web series</h2>
+//       <hr/>
+//       <UserClass name={'Rohini (class)'} location={'Gurgaon'} contact={'rohini19394@gmail.com'}/>
+//     </div>
+//   );
+// }
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Parent Constructor');
+  }
+  async componentDidMount() {
+    this.timer = setInterval(() => {
+      console.log('Namaste React Timer');
+    }, 1000);
+    console.log('Parent Component Did Mount'); // best place to make API calls
+  }
+
+  componentDidUpdate(){
+    console.log('Parent Component Did Update'); // this will called whenever there is a change in state or props of this component.
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer); // to clear the timer set in componentDidMount
+    console.log('Parent Component Will Unmount'); // this will only called when we navigate away from about page means this function is use for cleanup of current component.
+  }
+
+  render() {
+    console.log('Parent Render');
+    return (
+      <div className='component-top-spacing'>
+        <h1>About us</h1>
+        <h2>This is namaste React Web series</h2>
+        <hr />
+        <UserClass name={'Rohini (class)'} location={'Gurgaon'} contact={'rohini19394@gmail.com'} />
+      </div>
+    );
+  }
 }
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,18 +1,43 @@
 import React from "react";
 
 class UserClass extends React.Component {
-    constructor(props){
-        super(props);
+    constructor(props) {
+        super(props); // without super props, this.props will be undefined
 
-        console.log(props);
+        this.state = {
+            userInfo: {
+                name: 'Dummy',
+                location: 'Default',
+                contact: '0000000000',
+                avatar: 'https://dummyimage.com/600x400/000/fff'
+            },
+        }
+        // console.log('child - constructor');
     }
-    render(){
-        const {name, location, contact} = this.props;
+
+    async componentDidMount() {
+        // best place to make API calls
+        // console.log("componentDidMount - Child component");
+        const data = await fetch('https://api.github.com/users/rohiniee0028');
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo: json
+        })
+    }
+    render() {
+        const { name, location, bio, avatar_url } = this.state.userInfo;
+
+        // console.log('child - render');
         return (
             <div className="user-card">
+                <img src={avatar_url} alt="avatar" style={{ width: "150px", borderRadius: "75px" }} />
                 <h2>Name : {name}</h2>
                 <h3>Location : {location}</h3>
-                <h4>Contact : {contact}</h4>
+                <h4>Bio : {bio}</h4>
+                {/* <button onClick={() => {
+                    this.setState({ count: count + 1 })
+                }}>Increment Count</button> */}
             </div>
         )
     }
